ef8b2de278c3c372a0f34b95c33cd7fa79feb04b
Robert Smith <rssmith614@gmail.com>
2023-07-24 14:46:23 -0700

    ao5 and ao12 offcanvas summaries

--- src/components/TimeList/TimeList.js
+++ src/components/TimeList/TimeList.js
@@ -32,6 +32,114 @@ const TimeList = ({ times }) => {
     setOverlay(summary);
   }
 
+  const getAo5 = (idx) => {
+    const lastFive = times.sort((a, b) => (a.id - b.id)).slice(idx-5, idx);
+
+    const sorted = lastFive.slice().sort((a, b) => (a.time - b.time));
+    const smallest = sorted[0];
+    const largest = sorted[sorted.length-1];
+
+    const median = lastFive.filter(o => o !== smallest && o !== largest);
+
+    const ao5 = median.reduce((acc, curr) => acc + curr.time, 0) / 3;
+
+    const timeList = [];
+
+    lastFive.forEach(time => {
+      if (time === smallest || time === largest) {
+        timeList.push(
+          <p
+            data-bs-target="#offcanvasRight"
+            style={{ cursor: 'pointer' }}
+            onClick={() => selectTime(time.id)}>
+              <strong>{ time.id }:</strong>&nbsp;({msToStr(time.time)})
+          </p>
+        );
+      } else {
+        timeList.push(
+          <p
+            data-bs-target="#offcanvasRight"
+            style={{ cursor: 'pointer' }}
+            onClick={() => selectTime(time.id)}>
+              <strong>{ time.id }:</strong>&nbsp;{msToStr(time.time)}
+          </p>
+        );
+      }
+    });
+
+    timeList.reverse();
+
+    let summary = (
+      <>
+      <div className="offcanvas-header">
+        <div className="offcanvas-title h2">
+          Average of 5 Summary: {msToStr(ao5)}
+        </div>
+        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" />
+      </div>
+      <div className="offcanvas-body">
+        {timeList}
+      </div>
+      </>
+    );
+
+    setOverlay(summary);
+  }
+  
+  const getAo12 = (idx) => {
+    const lastTwelve = times.sort((a, b) => (a.id - b.id)).slice(idx-12, idx);
+
+    const sorted = lastTwelve.slice().sort((a, b) => (a.time - b.time));
+    const smallest = sorted[0];
+    const largest = sorted[sorted.length-1];
+
+    const median = lastTwelve.filter(o => o !== smallest && o !== largest);
+
+    const ao12 = median.reduce((acc, curr) => acc + curr.time, 0) / 9;
+
+    const timeList = [];
+
+    lastTwelve.forEach(time => {
+      if (time === smallest || time === largest) {
+        timeList.push(
+          <p
+            data-bs-target="#offcanvasRight"
+            style={{ cursor: 'pointer' }}
+            onClick={() => selectTime(time.id)}>
+              <strong>{ time.id }:</strong>&nbsp;({msToStr(time.time)})
+          </p>
+        );
+      } else {
+        timeList.push(
+          <p
+            data-bs-target="#offcanvasRight"
+            style={{ cursor: 'pointer' }}
+            onClick={() => selectTime(time.id)}>
+              <strong>{ time.id }:</strong>&nbsp;{msToStr(time.time)}
+          </p>
+        );
+      }
+    });
+
+    timeList.reverse();
+
+    let summary = (
+      <>
+      <div className="offcanvas-header">
+        <div className="offcanvas-title h2">
+          Average of 12 Summary: {msToStr(ao12)}
+        </div>
+        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" />
+      </div>
+      <div className="offcanvas-body">
+        {timeList}
+      </div>
+      </>
+    );
+
+    setOverlay(summary);
+  }
+
   function msToStr(milliseconds) {
     if (milliseconds === '') return "N/A";
     let mSeconds = Math.floor((milliseconds % 1000) );
@@ -60,8 +168,22 @@ const TimeList = ({ times }) => {
             </div>
           </td>
           <td>{ msToStr(time.time) }</td>
-          <td>{ msToStr(time.ao5) }</td>
-          <td>{ msToStr(time.ao12) }</td>
+          <td>
+            <div
+            data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+            style={{ cursor: 'pointer' }}
+            onClick={() => getAo5(time.id)}>
+              { msToStr(time.ao5) }
+            </div>
+          </td>
+          <td>
+            <div
+            data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+            style={{ cursor: 'pointer' }}
+            onClick={() => getAo12(time.id)}>
+              { msToStr(time.ao12) }
+            </div>
+          </td>
         </tr>
       );
     });
